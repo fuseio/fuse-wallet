18df5530b6c0a1173b7d8771ded99f733799b3e7
Leon Prouger <leonprou@gmail.com>
2019-12-19 15:34:23 +0200

    Pending fixes

--- lib/models/views/cash_wallet.dart
+++ lib/models/views/cash_wallet.dart
@@ -22,7 +22,7 @@ class CashWalletViewModel {
   final List<Transfer> tokenTransfers;
   final List<Transfer> pendingTransfers;
   final List<Contact> contacts;
-  final Map<String, String> reverseContracts;
+  final Map<String, String> reverseContacts;
   final String countryCode;
   final Function(String) createWallet;
   final Function() getWallet;
@@ -49,7 +49,7 @@ class CashWalletViewModel {
     this.pendingTransfers,
     this.contacts,
     this.countryCode,
-    this.reverseContracts,
+    this.reverseContacts,
     this.createWallet,
     this.getWallet,
     this.firstName,
@@ -76,7 +76,7 @@ class CashWalletViewModel {
       tokenTransfers: store.state.cashWalletState.tokenTransfers,
       pendingTransfers: store.state.cashWalletState.pendingTransfers,
       contacts: store.state.userState.contacts,
-      reverseContracts: store.state.userState.reverseContacts,
+      reverseContacts: store.state.userState.reverseContacts,
       countryCode: store.state.userState.countryCode,
       createWallet: (accountAddress) {
         store.dispatch(createAccountWalletCall(accountAddress));
--- lib/screens/cash_home/cash_transactions.dart
+++ lib/screens/cash_home/cash_transactions.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'dart:core';
 import 'dart:math';
 import 'package:fusecash/models/views/cash_wallet.dart';
 import 'package:fusecash/models/transfer.dart';
-import 'package:contacts_service/contacts_service.dart';  
+import 'package:contacts_service/contacts_service.dart';
 import 'package:fusecash/utils/phone.dart';
+import 'package:fusecash/utils/format.dart';
 
 class CashTransactios extends StatefulWidget {
   CashTransactios({@required this.viewModel});
@@ -23,42 +23,25 @@ String deduceSign(Transfer transfer) {
   }
 }
 
-String formatAddress(String address) {
-  return '${address.substring(0, 6)}...${address.substring(36, 42)}';
-}
-
-String deducePhoneNumber(Transfer transfer, Map<String, String> reverseContracts) {
+String deducePhoneNumber(
+    Transfer transfer, Map<String, String> reverseContacts) {
   String accountAddress = transfer.type == 'SEND' ? transfer.to : transfer.from;
-  if (reverseContracts.containsKey(accountAddress)) {
-    return reverseContracts[accountAddress];
+  if (reverseContacts.containsKey(accountAddress)) {
+    return reverseContacts[accountAddress];
   }
   return formatAddress(accountAddress);
 }
 
-// Future<Contact> getContact(Transfer transfer, CashWalletViewModel vm) async {
-//   String accountAddress = transfer.type == 'SEND' ? transfer.to : transfer.from;
-//   if (vm.reverseContracts.containsKey(accountAddress)) {
-//     String phoneNumber = vm.reverseContracts[accountAddress];
-//      ContactsService.getContactsForPhone(phoneNumber, withThumbnails: false).then((Iterable<Contact> contacts) {
-//       if (contacts.isEmpty) {
-//         return null;
-//       }
-//       return contacts.first;
-//      });
-
-//   }
-//   return null;
-// }
-
 
 Contact getContact(Transfer transfer, CashWalletViewModel vm) {
   String accountAddress = transfer.type == 'SEND' ? transfer.to : transfer.from;
-  if (vm.reverseContracts.containsKey(accountAddress.toLowerCase())) {
-    String phoneNumber = vm.reverseContracts[accountAddress.toLowerCase()];
+  if (vm.reverseContacts.containsKey(accountAddress.toLowerCase())) {
+    String phoneNumber = vm.reverseContacts[accountAddress.toLowerCase()];
     if (vm.contacts == null) return null;
     for (Contact contact in vm.contacts) {
       for (Item contactPhoneNumber in contact.phones.toList()) {
-        if (formatPhoneNumber(contactPhoneNumber.value, vm.countryCode) == phoneNumber) {
+        if (formatPhoneNumber(contactPhoneNumber.value, vm.countryCode) ==
+            phoneNumber) {
           return contact;
         }
       }
@@ -85,47 +68,43 @@ class CashTransactiosState extends State<CashTransactios> {
 
   @override
   Widget build(BuildContext _context) {
+    List<_TransactionListItem> transfers = this
+            .widget
+            .viewModel
+            .pendingTransfers
+            .map((transfer) => _TransactionListItem(
+                transfer,
+                getContact(transfer, this.widget.viewModel),
+                this.widget.viewModel,
+                true))
+            .toList() +
+        this
+            .widget
+            .viewModel
+            .tokenTransfers
+            .map((transfer) => _TransactionListItem(
+                transfer,
+                getContact(transfer, this.widget.viewModel),
+                this.widget.viewModel,
+                false))
+            .toList();
+
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Container(
-            padding: EdgeInsets.only(left: 15, top: 15, bottom: 8),
-            child: Text("Pending",
-                style: TextStyle(
-                    color: Color(0xFF979797),
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.normal))),
-        ListView(
-            shrinkWrap: true,
-            primary: false,
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            children: this
-                .widget
-                .viewModel
-                .pendingTransfers
-                .map((transfer) =>
-                    _TransactionListItem(transfer, getContact(transfer, this.widget.viewModel), this.widget.viewModel, ))
-                .toList()),
-        Container(
-            padding: EdgeInsets.only(left: 15, top: 15, bottom: 8),
-            child: Text("Transactions",
-                style: TextStyle(
-                    color: Color(0xFF979797),
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.normal))),
-        ListView(
-            shrinkWrap: true,
-            primary: false,
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            children: this
-                .widget
-                .viewModel
-                .tokenTransfers
-                .map((transfer) =>
-                    _TransactionListItem(transfer, getContact(transfer, this.widget.viewModel), this.widget.viewModel))
-                .toList())
-      ],
-    );
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.only(left: 15, top: 15, bottom: 8),
+              child: Text("Transactions",
+                  style: TextStyle(
+                      color: Color(0xFF979797),
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.normal))),
+          ListView(
+              shrinkWrap: true,
+              primary: false,
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              children: transfers)
+        ]);
   }
 }
 
@@ -133,11 +112,45 @@ class _TransactionListItem extends StatelessWidget {
   final Transfer _transfer;
   final Contact _contact;
   final CashWalletViewModel _vm;
+  final bool isPending;
 
-  _TransactionListItem(this._transfer, this._contact, this._vm);
+  _TransactionListItem(this._transfer, this._contact, this._vm, this.isPending);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> rightColumn = <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 5),
+            child: Text(
+              deduceSign(_transfer) + formatValue(_transfer.value, _vm.token.decimals),
+              style: TextStyle(
+                  color: deduceColor(_transfer),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Text(
+            " ${_vm.token.symbol}",
+            style: TextStyle(
+                color: deduceColor(_transfer),
+                fontSize: 18.0,
+                fontWeight: FontWeight.normal),
+          )
+        ],
+      )
+    ];
+
+    if (isPending) {
+      rightColumn.add(Padding(
+          child: Text("PENDING",
+              style: TextStyle(color: Color(0xFF8D8D8D), fontSize: 10)),
+          padding: EdgeInsets.only(top: 10)));
+    }
+
     return Container(
         decoration: new BoxDecoration(
             border: Border(bottom: BorderSide(color: const Color(0xFFDCDCDC)))),
@@ -147,10 +160,11 @@ class _TransactionListItem extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(_contact != null ? _contact.displayName : deducePhoneNumber(_transfer, _vm.reverseContracts),
+              Text(
+                  _contact != null
+                      ? _contact.displayName
+                      : deducePhoneNumber(_transfer, _vm.reverseContacts),
                   style: TextStyle(color: Color(0xFF333333), fontSize: 18))
-              // Text("For coffee",
-              //     style: TextStyle(color: Color(0xFF8D8D8D), fontSize: 15))
             ],
           ),
           leading: Stack(
@@ -164,33 +178,11 @@ class _TransactionListItem extends StatelessWidget {
           ),
           trailing: Container(
             width: 120,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 5),
-                  child: Text(
-                    deduceSign(_transfer) +
-                        (_transfer.value /
-                                BigInt.from(pow(10, _vm.token.decimals)))
-                            .toString(),
-                    style: TextStyle(
-                        color: deduceColor(_transfer),
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  " ${_vm.token.symbol}",
-                  style: TextStyle(
-                      color: deduceColor(_transfer),
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.normal),
-                )
-              ],
-            ),
-            padding: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: rightColumn),
+            padding: EdgeInsets.only(top: 10, bottom: 0, left: 10, right: 10),
           ),
         ));
   }
