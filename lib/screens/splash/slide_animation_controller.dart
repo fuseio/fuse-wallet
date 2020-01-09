9a9c5481bd8dc35c59daf5e0410a6e62d2159bec
Lior <liorag@colu.com>
2020-01-09 11:10:19 +0200

    added Eli's animation fix

--- lib/screens/splash/slide_animation_controller.dart
+++ lib/screens/splash/slide_animation_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flare_flutter/flare.dart';
 import 'package:flare_dart/math/mat2d.dart';
 import 'package:flare_flutter/flare_actor.dart';
@@ -30,25 +31,18 @@ class HouseController extends FlareController {
 
   @override
   bool advance(FlutterActorArtboard artboard, double elapsed) {
-    if (2 + _time.round() != 2 + step * 8) {
-      if (_time.round() < step * 8) {
+    var def = (2 + _time) - (2 + step2 * 8);
+    if (def > 0.5 || def < -0.5) {
+      if (_time.round() < step2 * 8) {
         _time += elapsed * _speed;
-        _arrange.apply(2 + _time, artboard, 1);
+        _arrange.apply(2 + _time, artboard, 0.5);
+        // print(_time);
       } else {
         _time -= elapsed * _speed;
-        _arrange.apply(2 + _time, artboard, 1);
+        _arrange.apply(2 + _time, artboard, 0.5);
+        // print(-_time);
       }
-
     }
-
-    //FlareAnimationLayer layer = (FlareAnimationLayer()..animation = _arrange);
-    //if (layer.time < 8) {
-
-    //layer.time += 2;
-    //layer.mix = min(1.0, layer.time / 0.07);
-    //layer.apply(artboard);
-
-    //}
 
     return true;
   }
@@ -63,92 +57,21 @@ class HouseController extends FlareController {
     _demoAnimation = FlareAnimationLayer()
       ..animation = _artboard.getAnimation("part1");
 
-/*var t = 0.0;
-    const oneSec = const Duration(seconds: 1);
-  Timer.periodic(
-    oneSec,
-    (Timer timer) => () {
-      t +=1;
-      print(t);
-      _arrange.apply(t, _artboard, 1);
-      if (t > 10) {
-        timer.cancel();
-      }
-    },
-  );*/
-
     _arrange.apply(2, _artboard, 1);
-
-    /*
-    
-    _skyAnimation = FlareAnimationLayer()
-      ..animation = _artboard.getAnimation("Sun Rotate")
-      ..mix = 1.0;
-    ActorAnimation endAnimation = artboard.getAnimation("to 6");
-    if (endAnimation != null) {
-      endAnimation.apply(endAnimation.duration, artboard, 1.0);
-    }
-    */
   }
 
   @override
   void setViewTransform(Mat2D viewTransform) {}
-/*
-  /// Use the [demoUpdated] callback to relay the current number of rooms
-  /// to the [Page] widget, so it can position the slider accordingly. 
-  _checkRoom() {
-    double demoFrame = _demoAnimation.time * FPS;
-    double demoValue = 0.0;
-    if (demoFrame <= 15) {
-      demoValue =
-          lerpDouble(6.0, 5.0, Curves.easeInOut.transform(demoFrame / 15));
-    } else if (demoFrame <= 36) {
-      demoValue = 5.0;
-    } else if (demoFrame <= 50) {
-      demoValue = lerpDouble(
-          5.0, 4.0, Curves.easeInOut.transform((demoFrame - 36) / (50 - 36)));
-    } else if (demoFrame <= 72) {
-      demoValue = 4.0;
-    } else if (demoFrame <= 87) {
-      demoValue = lerpDouble(
-          4.0, 3.0, Curves.easeInOut.transform((demoFrame - 72) / (87 - 72)));
-    } else if (demoFrame <= 128) {
-      demoValue = 3.0;
-    } else if (demoFrame <= 142) {
-      demoValue = lerpDouble(3.0, 6.0,
-          Curves.easeInOut.transform((demoFrame - 128) / (142 - 128)));
-    } else if (demoFrame <= 164) {
-      demoValue = 6.0;
-    }
-    if (_lastDemoValue != demoValue) {
-      _lastDemoValue = demoValue;
-      this._rooms = demoValue.toInt();
-      /// Use the callback to let the [Page] widget know that the current value
-      /// has been changed so that the Slider can be updated.
-      if (demoUpdated != null) {
-        demoUpdated();
-      }
-    }
-    
-  }
-*/
-
-/*
-  _enqueueAnimation(String name) {
-    ActorAnimation animation = _artboard.getAnimation(name);
-    if (animation != null) {
-      _roomAnimations.add(FlareAnimationLayer()..animation = animation);
-    }
-  }
-*/
 
   Timer _timer;
   var currentStep = 0.0;
   var frame = 0.0;
   var step = 0;
+  var step2 = 0.0;
 
   set rooms(double value) {
     step = value.round();
+    step2 = value;
 
     return;
 
@@ -166,7 +89,7 @@ class HouseController extends FlareController {
         var s = 2 + frame;
         _arrange.apply(s, _artboard, 1);
 
-        print(s);
+        // print(s);
 
         if (nextStep >= currentStep) {
           if (s > 2 + (nextStep) * 8) {
