2c221d34d8630e524698dc9092802fc7b683ec9b
Lior <liorag@colu.com>
2020-01-13 11:05:25 +0200

    fixed split bug

--- lib/redux/state/store.dart
+++ lib/redux/state/store.dart
@@ -1,8 +1,6 @@
-
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fusecash/models/app_state.dart';
-import 'package:fusecash/redux/actions/user_actions.dart' as prefix0;
 import 'package:fusecash/redux/reducers/app_reducer.dart';
 import 'package:fusecash/redux/state/state_secure_storage.dart';
 import 'package:redux_persist/redux_persist.dart';
@@ -18,46 +16,43 @@ import 'package:path_provider/path_provider.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_segment/flutter_segment.dart';
 
-
-
 Future<File> getFile() async {
   final directory = await getApplicationDocumentsDirectory();
-  return File(directory.path+"/logs.txt");
+  return File(directory.path + "/logs.txt");
 }
 
 class ConsoleOutput extends logger_package.LogOutput {
-
   File file;
   ConsoleOutput(File _file) {
     file = _file;
   }
 
-
   @override
   void output(logger_package.OutputEvent event) async {
     for (var line in event.lines) {
       print(line);
-       await file.writeAsString(line + '\n', mode: FileMode.append);
+      await file.writeAsString(line + '\n', mode: FileMode.append);
     }
   }
 }
 
 Future<Store<AppState>> createReduxStore() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   var storage = new FlutterSecureStorage();
 
   File file = await getFile();
   var output = new ConsoleOutput(file);
 
   var logger = logger_package.Logger(
-    printer: logger_package.PrettyPrinter(),
-    output: output
+      printer: logger_package.PrettyPrinter(),
+      output: output
   );
 
   final persistor = Persistor<AppState>(
     //storage: FlutterStorage(key: "app6"),
-    storage: SecureStorage(storage= storage),
+    storage: SecureStorage(storage = storage),
     serializer: JsonSerializer<AppState>(AppState.fromJson),
+    debug: DotEnv().env['MODE'] == 'development'
   );
 
   AppState initialState;
@@ -70,58 +65,53 @@ Future<Store<AppState>> createReduxStore() async {
     } else {
       logger.d('no JWT');
     }
-
-  }
-  catch (e) {
+  } catch (e) {
     logger.e(e);
     initialState = null;
   }
-  
 
   // logger.onRecord
   final mylogger = new Logger("Redux Logger");
 
-    mylogger.onRecord
-    // Filter down to [LogRecord]s sent to your logger instance
-    .where((record) => record.loggerName == mylogger.name)
-    // Print them out (or do something more interesting!)
-    . listen((loggingMiddlewareRecord) => logger.d(loggingMiddlewareRecord));
+  mylogger.onRecord
+      // Filter down to [LogRecord]s sent to your logger instance
+      .where((record) => record.loggerName == mylogger.name)
+      // Print them out (or do something more interesting!)
+      .listen((loggingMiddlewareRecord) => logger.d(loggingMiddlewareRecord));
 
-    FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
 
-    void iOS_Permission() {
-      firebaseMessaging.requestNotificationPermissions(
-          IosNotificationSettings(sound: true, badge: true, alert: true)
-      );
-      firebaseMessaging.onIosSettingsRegistered
-          .listen((IosNotificationSettings settings)
-      {
-        print("Settings registered: $settings");
-      });
-    }
+  void iosPermission() {
+    var firebaseMessaging2 = firebaseMessaging;
+    firebaseMessaging2.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true));
+    firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+  }
 
-
-    if (Platform.isIOS) iOS_Permission();
-    var token = await firebaseMessaging.getToken();
-    logger.wtf("token $token");
-    await FlutterSegment.putDeviceToken(token);
-    firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) {
-        logger.wtf('onMessage called: $message');
-      },
-      onResume: (Map<String, dynamic> message) {
-        logger.wtf('onResume called: $message');
-      },
-      onLaunch: (Map<String, dynamic> message) {
-        logger.wtf('onLaunch called: $message');
-      },
-    );
-    return Store<AppState>(
-      appReducer,
-      initialState: initialState ?? new AppState.initial(),
-        // middleware: [thunkMiddleware, persistor.createMiddleware()]
-      middleware: [thunkMiddleware, new LoggingMiddleware(logger: mylogger), persistor.createMiddleware()]
+  if (Platform.isIOS) iosPermission();
+  var token = await firebaseMessaging.getToken();
+  logger.wtf("token $token");
+  await FlutterSegment.putDeviceToken(token);
+  firebaseMessaging.configure(
+    onMessage: (Map<String, dynamic> message) async {
+      logger.wtf('onMessage called: $message');
+    },
+    onResume: (Map<String, dynamic> message) async {
+      logger.wtf('onResume called: $message');
+    },
+    onLaunch: (Map<String, dynamic> message) async {
+      logger.wtf('onLaunch called: $message');
+    },
   );
-
-
+  return Store<AppState>(appReducer,
+      initialState: initialState ?? new AppState.initial(),
+      // middleware: [thunkMiddleware, persistor.createMiddleware()]
+      middleware: [
+        thunkMiddleware,
+        new LoggingMiddleware(logger: mylogger),
+        persistor.createMiddleware()
+      ]);
 }
