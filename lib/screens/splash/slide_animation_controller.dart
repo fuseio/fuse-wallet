661e8ab897cca1cd12523ef0d2bb62df31e2ea6e
Eli Smargon <elismargon@gmail.com>
2020-01-31 16:11:43 +0200

    animation

--- lib/screens/splash/slide_animation_controller.dart
+++ lib/screens/splash/slide_animation_controller.dart
@@ -15,53 +15,42 @@ class HouseController extends FlareController {
 
   bool isDemoMode = true;
   double _rooms = 0;
-  // double _lastDemoValue = 0.0;
   FlutterActorArtboard _artboard;
-  // FlareAnimationLayer _demoAnimation;
-  // FlareAnimationLayer _skyAnimation;
-
-  // final List<FlareAnimationLayer> _roomAnimations = [];
 
   ActorAnimation _arrange;
 
   double _time = 0;
-  double _speed = 8;
+  double _speed = 4;
+  double _startFrom = 2;
+  double _animationLength = 7.5;
 
   @override
   bool advance(FlutterActorArtboard artboard, double elapsed) {
-    var def = (2 + _time) - (2 + step2 * 8);
+
+    var def = (_startFrom + _time) - (_startFrom + step2 * _animationLength);
     if (def > 0.5 || def < -0.5) {
-      if (_time.round() < step2 * 8) {
+      if (_time.round() < step2 * _animationLength) {
         _time += elapsed * _speed;
-        _arrange.apply(2 + _time, artboard, 0.5);
-        // print(_time);
+        _arrange.apply(_startFrom + _time, artboard, 0.5);
       } else {
         _time -= elapsed * _speed;
-        _arrange.apply(2 + _time, artboard, 0.5);
-        // print(-_time);
+        _arrange.apply(_startFrom + _time, artboard, 0.5);
       }
     }
 
     return true;
   }
 
-  /// Grab the references to the right animations, and
-  /// packs them into [FlareAnimationLayer] objects
   @override
   void initialize(FlutterActorArtboard artboard) {
-    _arrange = artboard.getAnimation("part1");
-
+    _arrange = artboard.getAnimation("Animation");
     _artboard = artboard;
-    // _demoAnimation = FlareAnimationLayer()
-      // ..animation = _artboard.getAnimation("part1");
-
-    _arrange.apply(2, _artboard, 1);
+    _arrange.apply(_startFrom, _artboard, 1);
   }
 
   @override
   void setViewTransform(Mat2D viewTransform) {}
 
-  // Timer _timer;
   var currentStep = 0.0;
   var frame = 0.0;
   var step = 0;
@@ -70,42 +59,6 @@ class HouseController extends FlareController {
   set rooms(double value) {
     step = value.round();
     step2 = value;
-
-    return;
-
-    // var nextStep = (value % 8).round().toDouble();
-
-    // if (_timer == null || !_timer.isActive) {
-    //   _timer =
-    //       new Timer.periodic(const Duration(microseconds: 400), (Timer timer) {
-    //     if (nextStep >= currentStep) {
-    //       frame += 0.003;
-    //     } else {
-    //       frame -= 0.003;
-    //     }
-
-    //     var s = 2 + frame;
-    //     _arrange.apply(s, _artboard, 1);
-
-    //     // print(s);
-
-    //     if (nextStep >= currentStep) {
-    //       if (s > 2 + (nextStep) * 8) {
-    //         currentStep = nextStep;
-    //         timer.cancel();
-    //       }
-    //     } else {
-    //       if (s < 2 + (nextStep) * 8) {
-    //         currentStep = nextStep;
-    //         timer.cancel();
-    //       }
-    //     }
-    //   });
-    // }
-
-    //var time = 2 + (value * 8).abs();
-    //print(time);
-    //_arrange.apply(time, _artboard, 1);
   }
 
   double get rooms => _rooms;
