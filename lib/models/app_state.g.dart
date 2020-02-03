242e218f8b1d0b8fa4578ceed07b8d42ea4e576b
Lior Agnin <Lior.Agnin@gmail.com>
2020-02-03 10:25:34 +0200

    json_serializable (#116)

---
+++ lib/models/app_state.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AppState _$AppStateFromJson(Map<String, dynamic> json) {
+  return AppState(
+    userState: json['userState'] == null
+        ? null
+        : UserState.fromJson(json['userState'] as Map<String, dynamic>),
+    cashWalletState: json['cashWalletState'] == null
+        ? null
+        : CashWalletState.fromJson(
+            json['cashWalletState'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$AppStateToJson(AppState instance) => <String, dynamic>{
+      'userState': instance.userState?.toJson(),
+      'cashWalletState': instance.cashWalletState?.toJson(),
+    };
--- lib/models/community.g.dart
+++ lib/models/community.g.dart
@@ -11,12 +11,15 @@ Community _$CommunityFromJson(Map<String, dynamic> json) {
     name: json['name'] as String,
     isMember: json['isMember'] as bool,
     address: json['address'] as String,
-    plugins:
-        Community._pluginsFromJson(json['plugins'] as Map<String, dynamic>),
+    plugins: json['plugins'] == null
+        ? null
+        : Plugins.fromJson(json['plugins'] as Map<String, dynamic>),
     transactions: json['transactions'] == null
         ? null
         : Transactions.fromJson(json['transactions'] as Map<String, dynamic>),
-    token: Community._tokenFromJson(json['token'] as Map<String, dynamic>),
+    token: json['token'] == null
+        ? null
+        : Token.fromJson(json['token'] as Map<String, dynamic>),
     tokenBalance: json['tokenBalance'] == null
         ? null
         : BigInt.parse(json['tokenBalance'] as String),
@@ -25,8 +28,9 @@ Community _$CommunityFromJson(Map<String, dynamic> json) {
             e == null ? null : Business.fromJson(e as Map<String, dynamic>))
         ?.toList(),
     jobs: Community._jobsFromJson(json['jobs'] as Map<String, dynamic>),
-    metadata: Community._communityMetadataFromJson(
-        json['metadata'] as Map<String, dynamic>),
+    metadata: json['metadata'] == null
+        ? null
+        : CommunityMetadata.fromJson(json['metadata'] as Map<String, dynamic>),
   );
 }
 
@@ -37,8 +41,8 @@ Map<String, dynamic> _$CommunityToJson(Community instance) => <String, dynamic>{
       'isMember': instance.isMember,
       'businesses': instance.businesses?.map((e) => e?.toJson())?.toList(),
       'transactions': instance.transactions?.toJson(),
-      'token': Community._tokenToJson(instance.token),
+      'token': instance.token?.toJson(),
+      'plugins': instance.plugins?.toJson(),
+      'metadata': instance.metadata?.toJson(),
       'jobs': Community._jobsToJson(instance.jobs),
-      'plugins': Community._pluginsToJson(instance.plugins),
-      'metadata': Community._communityMetadataToJson(instance.metadata),
     };
