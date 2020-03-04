bdb9214ba732539f181f4773d64661f2205c668f
Tal Beja <tal@fuse.io>
2020-03-04 15:29:05 +0200

    add created status check

--- lib/screens/cash_home/cash_home.dart
+++ lib/screens/cash_home/cash_home.dart
@@ -48,7 +48,7 @@ void onChange(CashWalletViewModel viewModel, BuildContext context,
       viewModel.walletAddress != '') {
     viewModel.branchCommunityUpdate();
   }
-  if (viewModel.walletStatus != 'deploying' && viewModel.accountAddress != '') {
+  if (viewModel.walletStatus != 'deploying' && viewModel.walletStatus != 'created' && viewModel.accountAddress != '') {
     viewModel.createWallet(viewModel.accountAddress);
   }
   if (!viewModel.isCommunityLoading &&
