3a7c2c67fba09f57cc3f0f5ae34f599d63e24068
Lior <lior.agnin@gmail.com>
2020-03-24 19:24:03 +0200

    added fake pending

--- lib/models/pro/views/pro_wallet.dart
+++ lib/models/pro/views/pro_wallet.dart
@@ -1,27 +1,42 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:fusecash/models/community.dart';
 import 'package:fusecash/models/pro/token.dart';
+import 'package:fusecash/models/transactions/transfer.dart';
+import 'package:fusecash/utils/addresses.dart';
 import 'package:redux/redux.dart';
 import 'package:fusecash/models/app_state.dart';
 
 class ProWalletViewModel extends Equatable {
   final String walletAddress;
   final List<Token> tokens;
+  final bool hasTrasnferdToForeign;
 
   ProWalletViewModel({
     this.walletAddress,
+    this.hasTrasnferdToForeign,
     this.tokens
   });
 
   static ProWalletViewModel fromStore(Store<AppState> store) {
+    List<Token> tokens = store.state.proWalletState?.tokens ?? [];
+    String communityAddres = DotEnv().env['DEFAULT_COMMUNITY_CONTRACT_ADDRESS'].toLowerCase();
+    Community community = store.state.cashWalletState.communities[communityAddres];
+    bool hasTrasnferdToForeign = community.transactions.list.any((item) {
+        Transfer transfer = item as Transfer;
+        return (transfer?.to?.toLowerCase() == community?.homeBridgeAddress?.toLowerCase()) ?? false;
+      }) && !tokens.any((token) => token?.address == daiTokenAddress);
     return ProWalletViewModel(
+      hasTrasnferdToForeign: hasTrasnferdToForeign,
       walletAddress: store.state.userState.walletAddress,
-      tokens: store.state.proWalletState?.tokens ?? []
+      tokens: tokens,
     );
   }
 
   @override
   List<Object> get props => [
     walletAddress,
-    tokens
+    tokens,
+    hasTrasnferdToForeign
   ];
 }
--- lib/screens/pro_mode/assets_list.dart
+++ lib/screens/pro_mode/assets_list.dart
@@ -6,6 +6,14 @@ import 'package:fusecash/models/pro/token.dart';
 import 'package:fusecash/models/pro/views/pro_wallet.dart';
 import 'package:fusecash/utils/addresses.dart';
 
+final Token daiToken = Token(
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    decimals: 18,
+    imageUrl:
+        "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+    name: "Dai Stablecoin",
+    symbol: "DAI");
+
 class AssetsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,6 +35,11 @@ class AssetsList extends StatelessWidget {
                     primary: false,
                     padding: EdgeInsets.only(left: 15, right: 15),
                     children: [
+                      viewModel.hasTrasnferdToForeign
+                          ? _TokenPendingRow(
+                              token: daiToken,
+                            )
+                          : SizedBox.shrink(),
                       ...viewModel.tokens.reversed
                           .map((Token token) => _TokenRow(
                                 token: token,
@@ -136,6 +149,97 @@ class _TokenRow extends StatelessWidget {
                                                     fontSize: 10)),
                                             padding: EdgeInsets.only(top: 10)))
                                     : SizedBox.shrink()
+                              ],
+                            )
+                          ],
+                        )
+                      ]))
+            ],
+          )),
+    );
+  }
+}
+
+class _TokenPendingRow extends StatelessWidget {
+  _TokenPendingRow({this.token});
+  final Token token;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+          border: Border(bottom: BorderSide(color: const Color(0xFFDCDCDC)))),
+      child: ListTile(
+          contentPadding: EdgeInsets.only(top: 8, bottom: 8, left: 0, right: 0),
+          title: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Flexible(
+                  flex: 12,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Flexible(
+                        flex: 4,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: <Widget>[
+                            Hero(
+                              child: CircleAvatar(
+                                backgroundColor: Color(0xFFE0E0E0),
+                                radius: 27,
+                                backgroundImage: NetworkImage(
+                                  token.imageUrl,
+                                ),
+                              ),
+                              tag: token.name,
+                            ),
+                            Container(
+                                width: 55,
+                                height: 55,
+                                child: CircularProgressIndicator(
+                                  backgroundColor:
+                                      Color(0xFF49D88D).withOpacity(0),
+                                  strokeWidth: 3,
+                                  valueColor: new AlwaysStoppedAnimation<Color>(
+                                      Color(0xFF49D88D).withOpacity(1)),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        flex: 10,
+                        child: Text(token.name,
+                            style: TextStyle(
+                                color: Color(0xFF333333), fontSize: 15)),
+                      ),
+                    ],
+                  )),
+              Flexible(
+                  flex: 4,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Stack(
+                              overflow: Overflow.visible,
+                              alignment: AlignmentDirectional.bottomEnd,
+                              children: <Widget>[
+                                new RichText(
+                                    text: new TextSpan(children: <TextSpan>[
+                                  new TextSpan(
+                                      text: I18n.of(context).pending,
+                                      style: new TextStyle(
+                                          fontSize: 13.0,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary)),
+                                ]))
                               ],
                             )
                           ],
