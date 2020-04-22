88e2804bd75b05060a059951837487a89e17a827
Lior <lior.agnin@gmail.com>
2020-04-22 17:56:03 +0300

    fixed phonenumbers

--- lib/utils/phone.dart
+++ lib/utils/phone.dart
@@ -1,11 +1,22 @@
 String formatPhoneNumber (String phoneNumber, String myCountryCode) {
+  phoneNumber = removeUnicodes(phoneNumber);
+  myCountryCode = removeUnicodes(myCountryCode);
   String countryCodeNumeric = myCountryCode.replaceFirst('+', '');
   phoneNumber = phoneNumber.replaceFirst(myCountryCode, '');
   phoneNumber = phoneNumber.startsWith(countryCodeNumeric) ? phoneNumber.replaceFirst(countryCodeNumeric, '') : phoneNumber;
   phoneNumber = phoneNumber.replaceAll(new RegExp('(-| |\\(0\\)|\\(0|\\(|\\))'), '');
   phoneNumber = phoneNumber.replaceFirst(new RegExp('^0+'), '');
   if (!phoneNumber.startsWith('+')) {
-    phoneNumber = myCountryCode + phoneNumber;
+    if (myCountryCode.contains('+')) {
+      phoneNumber = myCountryCode + phoneNumber;
+    } else {
+      myCountryCode = '+$myCountryCode';
+      phoneNumber = myCountryCode + phoneNumber;
+    }
   }
   return phoneNumber;
+}
+
+String removeUnicodes (String value) {
+  return value.replaceAll(new RegExp('/\u200F|\u200E/g'), '');
 }
