c87f10369e38293d6282ee8c956efbefed57a173
Lior Agnin <lior.agnin@gmail.com>
2020-07-05 10:23:02 +0300

    minor fix

--- lib/screens/cash_home/cash_home.dart
+++ lib/screens/cash_home/cash_home.dart
@@ -33,33 +33,36 @@ class _CashHomeScreenState extends State<CashHomeScreen> {
                       appBar: MyAppBar(
                         backgroundColor: Theme.of(context).splashColor,
                         child: Padding(
-                          padding: EdgeInsets.only(top: 20, bottom: 10),
-                          child: TabBar(
-                            indicator: BoxDecoration(color: Colors.white),
-                            onTap: (int index) {
-                              setState(() {});
-                            },
-                            unselectedLabelStyle: TextStyle(
-                                backgroundColor: Theme.of(context).splashColor),
-                            tabs: tabsTitles.asMap().entries.map((title) {
-                              bool isSeleceted;
-                              if (DefaultTabController.of(context).index !=
-                                  null) {
-                                isSeleceted = title.key ==
-                                    DefaultTabController.of(context).index;
-                              }
-                              return Chip(
-                                labelPadding: EdgeInsets.only(
-                                    top: 2, bottom: 2, right: 50, left: 50),
-                                label: Text(title.value),
-                                shadowColor: Theme.of(context).splashColor,
-                                backgroundColor: isSeleceted
-                                    ? Color(0xFFF2F2F2)
-                                    : Theme.of(context).splashColor,
-                              );
-                            }).toList(),
-                          ),
-                        ),
+                            padding: EdgeInsets.only(top: 20, bottom: 10),
+                            child: Container(
+                              color: Theme.of(context).splashColor,
+                              child: TabBar(
+                                indicator: BoxDecoration(color: Colors.white),
+                                onTap: (int index) {
+                                  setState(() {});
+                                },
+                                unselectedLabelStyle: TextStyle(
+                                    backgroundColor:
+                                        Theme.of(context).splashColor),
+                                tabs: tabsTitles.asMap().entries.map((title) {
+                                  bool isSeleceted;
+                                  if (DefaultTabController.of(context).index !=
+                                      null) {
+                                    isSeleceted = title.key ==
+                                        DefaultTabController.of(context).index;
+                                  }
+                                  return Chip(
+                                    labelPadding: EdgeInsets.only(
+                                        top: 2, bottom: 2, right: 50, left: 50),
+                                    label: Text(title.value),
+                                    shadowColor: Theme.of(context).splashColor,
+                                    backgroundColor: isSeleceted
+                                        ? Color(0xFFF2F2F2)
+                                        : Theme.of(context).splashColor,
+                                  );
+                                }).toList(),
+                              ),
+                            )),
                       ),
                       body: TabBarView(
                         children: [Feed(), AssetsList()],
