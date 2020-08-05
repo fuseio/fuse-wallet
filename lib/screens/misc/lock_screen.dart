99c6130ad5f336a604b44ae2ce833d41f3e1b05a
Lior Agnin <lior.agnin@gmail.com>
2020-08-05 13:22:43 +0300

    minor

--- lib/screens/misc/lock_screen.dart
+++ lib/screens/misc/lock_screen.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:fusecash/models/views/backup.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fusecash/models/app_state.dart';
 import 'package:fusecash/models/user_state.dart';
 import 'package:fusecash/screens/routes.gr.dart';
