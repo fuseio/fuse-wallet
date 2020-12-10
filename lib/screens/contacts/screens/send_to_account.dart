645c2624b7fb5b5b42bcc2c2c5b59f58969d8d2d
Lior Agnin <lior.agnin@gmail.com>
2020-12-10 16:26:31 +0200

    send community address to create wallet

---
+++ lib/screens/contacts/screens/send_to_account.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:fusecash/generated/i18n.dart';
+import 'package:fusecash/screens/contacts/widgets/contact_tile.dart';
+import 'package:fusecash/utils/format.dart';
+import 'package:fusecash/utils/send.dart';
+
+class SendToAccount extends StatelessWidget {
+  final String accountAddress;
+  final VoidCallback resetSearch;
+  const SendToAccount({Key key, this.accountAddress, this.resetSearch})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        ContactTile(
+          displayName: formatAddress(accountAddress),
+          onTap: () {
+            resetSearch();
+            sendToPastedAddress(accountAddress);
+          },
+          trailing: InkWell(
+            child: Text(
+              I18n.of(context).next_button,
+              style: TextStyle(color: Color(0xFF0377FF)),
+            ),
+            onTap: () {
+              resetSearch();
+              sendToPastedAddress(accountAddress);
+            },
+          ),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/screens/contacts/widgets/list_header.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:fusecash/widgets/silver_app_bar.dart';
+
+class ListHeader extends StatelessWidget {
+  final String title;
+  const ListHeader({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: true,
+      floating: true,
+      delegate: SliverAppBarDelegate(
+        minHeight: 40.0,
+        maxHeight: 40.0,
+        child: Container(
+          color: Color(0xFFF8F8F8),
+          padding: EdgeInsets.only(left: 20, top: 7),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
