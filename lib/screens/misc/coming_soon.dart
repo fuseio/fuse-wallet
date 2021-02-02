ce83a66baabe9c3db0be5c60515e1b8989b22328
Lior Agnin <lior.agnin@gmail.com>
2021-02-02 14:54:24 +0200

    minor

--- lib/screens/misc/coming_soon.dart
+++ lib/screens/misc/coming_soon.dart
@@ -7,25 +7,20 @@ class SwapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MainScaffold(
-        automaticallyImplyLeading: false,
-        title: I18n.of(context).swap,
-        children: <Widget>[
-          Image.asset(
-            'assets/images/swap-illustr.png',
+      automaticallyImplyLeading: false,
+      title: I18n.of(context).coming_soon,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 30, right: 30, top: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                  'Soon you will be able to swap your Fuse Dollars to other currencies with the best experience in the industry and no gas!')
+            ],
           ),
-          Container(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  I18n.of(context).coming_soon,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
-                  ),
-                )
-              ],
-            ),
-          )
-        ]);
+        )
+      ],
+    );
   }
 }
