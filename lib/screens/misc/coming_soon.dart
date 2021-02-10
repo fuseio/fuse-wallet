87a12a087ae3a2b994fb8cf6b6b42beef21cb916
Lior Agnin <lior.agnin@gmail.com>
2021-02-10 13:25:15 +0200

    update coming soon page UI

--- lib/screens/misc/coming_soon.dart
+++ lib/screens/misc/coming_soon.dart
@@ -1,5 +1,6 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fusecash/generated/i18n.dart';
 import 'package:fusecash/widgets/main_scaffold.dart';
 
@@ -10,6 +11,56 @@ class SwapScreen extends StatelessWidget {
       automaticallyImplyLeading: false,
       title: I18n.of(context).coming_soon,
       children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Stack(
+              overflow: Overflow.visible,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * .9,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFF153858),
+                        Color(0xFF0A0E13),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/fuseswap_logo.png'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SvgPicture.asset('assets/images/soon.svg'),
+                    ],
+                  ),
+                ),
+                Positioned.fill(
+                  top: -45,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      'assets/images/swap_logo.png',
+                      width: 90,
+                      height: 90,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
         Container(
           padding: EdgeInsets.only(left: 30, right: 30, top: 30),
           child: Column(
