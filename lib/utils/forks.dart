f435a11feb4aef6f15c036ce8214b98d5bbd27b4
Lior Agnin <lior.agnin@gmail.com>
2021-04-27 14:08:14 +0300

    update community address

--- lib/utils/forks.dart
+++ lib/utils/forks.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final String wikBankCommunityAddress =
-    '0xff4e4b6283db9e1d4690160a0cf740736e2b3ee8';
+    '0x60235b86f5acea7146a311eb77873be6d0189b7c';
 
 bool isFork() {
   return DotEnv().env['FORK'] != null && DotEnv().env['FORK'] == 'true';
