fbed146d2091951bf8f501700298a0db8ef3197d
Lior Agnin <lior.agnin@gmail.com>
2021-05-02 15:23:25 +0300

    minor

--- lib/redux/viewsmodels/trade_card.dart
+++ lib/redux/viewsmodels/trade_card.dart
@@ -1,10 +1,6 @@
 import 'package:fusecash/models/app_state.dart';
-import 'package:fusecash/models/plugins/plugins.dart';
-import 'package:fusecash/models/tokens/token.dart';
-import 'package:fusecash/utils/format.dart';
 import 'package:redux/redux.dart';
 import 'package:equatable/equatable.dart';
-import 'package:fusecash/models/community/community.dart';
 
 class TradeCardViewModel extends Equatable {
   final Map<String, String> tokensImages;
