3e261fd8c085c051eca618599aee0a8d6d8fcc14
Lior Agnin <lior.agnin@gmail.com>
2021-07-05 10:56:33 +0300

    added branch.io

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,18 @@
-// import 'dart:async';
+import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:country_code_picker/country_localizations.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
+import 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_segment/flutter_segment.dart';
 import 'package:fusecash/common/router/route_guards.dart';
 import 'package:fusecash/constants/strings.dart';
 import 'package:fusecash/generated/l10n.dart';
 import 'package:fusecash/models/app_state.dart';
-// import 'package:fusecash/redux/actions/cash_wallet_actions.dart';
+import 'package:fusecash/redux/actions/cash_wallet_actions.dart';
 import 'package:fusecash/services.dart';
 import 'package:fusecash/utils/log/log.dart';
 import 'package:redux/redux.dart';
@@ -32,7 +33,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // late StreamSubscription<Map> streamSubscription;
+  late StreamSubscription<Map> streamSubscription;
   Locale? _locale;
   setLocale(Locale locale) {
     setState(() {
@@ -49,62 +50,59 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  // void listenDynamicLinks(Store<AppState> store) async {
-  //   streamSubscription =
-  //       FlutterBranchSdk.initSession().listen((linkData) async {
-  //     log.info("branch listening.");
-  //     store.dispatch(BranchListening());
-  //     log.info("Got link data: ${linkData.toString()}");
-  //     if (linkData["~feature"] == "switch_community") {
-  //       final String communityAddress = linkData['community_address'];
-  //       log.info("communityAddress $communityAddress");
-  //       store.dispatch(BranchCommunityToUpdate(communityAddress));
-  //       store.dispatch(
-  //         segmentIdentifyCall(
-  //           Map<String, dynamic>.from({
-  //             'Referral': linkData["~feature"],
-  //             'Referral link': linkData['~referring_link']
-  //           }),
-  //         ),
-  //       );
-  //       // store.dispatch(
-  //       //   segmentTrackCall(
-  //       //     "Wallet: Branch: Studio Invite",
-  //       //     properties: Map<String, dynamic>.from(linkData),
-  //       //   ),
-  //       // );
-  //     }
-  //     if (linkData["~feature"] == "invite_user") {
-  //       final String communityAddress = linkData["community_address"];
-  //       store.dispatch(BranchCommunityToUpdate(communityAddress));
-  //       store.dispatch(
-  //         segmentIdentifyCall(
-  //           Map<String, dynamic>.from({
-  //             'Referral': linkData["~feature"],
-  //             'Referral link': linkData['~referring_link']
-  //           }),
-  //         ),
-  //       );
-  //       // store.dispatch(
-  //       //   segmentTrackCall(
-  //       //     "Wallet: Branch: User Invite",
-  //       //     properties: Map<String, dynamic>.from(linkData),
-  //       //   ),
-  //       // );
-  //     }
-  //   });
-  // }
+  void listenDynamicLinks(Store<AppState> store) async {
+    streamSubscription =
+        FlutterBranchSdk.initSession().listen((linkData) async {
+      log.info("branch listening.");
+      store.dispatch(BranchListening());
+      log.info("Got link data: ${linkData.toString()}");
+      if (linkData["~feature"] == "switch_community") {
+        final String communityAddress = linkData['community_address'];
+        log.info("communityAddress $communityAddress");
+        store.dispatch(BranchCommunityToUpdate(communityAddress));
+        store.dispatch(
+          segmentIdentifyCall(
+            Map<String, dynamic>.from({
+              'Referral': linkData["~feature"],
+              'Referral link': linkData['~referring_link']
+            }),
+          ),
+        );
+        Segment.track(
+          eventName: 'Wallet: Branch: Studio Invite',
+          properties: Map<String, dynamic>.from(linkData),
+        );
+      }
+      if (linkData["~feature"] == "invite_user") {
+        final String communityAddress = linkData["community_address"];
+        store.dispatch(BranchCommunityToUpdate(communityAddress));
+        store.dispatch(
+          segmentIdentifyCall(
+            Map<String, dynamic>.from({
+              'Referral': linkData["~feature"],
+              'Referral link': linkData['~referring_link']
+            }),
+          ),
+        );
+        Segment.track(
+          eventName: 'Wallet: Branch: User Invite',
+          properties: Map<String, dynamic>.from(linkData),
+        );
+      }
+    });
+  }
 
-  // @override
-  // void dispose() {
-  //   streamSubscription.cancel();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    streamSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     setJwtToken(widget.store);
+    listenDynamicLinks(widget.store);
     _locale = widget.store.state.userState.locale;
   }
 
