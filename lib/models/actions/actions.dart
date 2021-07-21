61d58f2133281d4f007f8c5a4a4f8d3cb6ed7a8a
Lior Agnin <lior.agnin@gmail.com>
2021-07-21 13:44:16 +0300

    m

--- lib/models/actions/actions.dart
+++ lib/models/actions/actions.dart
@@ -1,4 +1,5 @@
 import 'package:fusecash/models/actions/wallet_action.dart';
+import 'package:fusecash/utils/log/log.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
